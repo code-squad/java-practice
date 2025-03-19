9fd2e0a2c0cc45b86829c1748cc4023d1d1a519f
Hoyoung Jung <honux77@gmail.com>
2025-03-19 12:36:06 +0900

    2025 버전업

--- src/test/java/next/reflection/Junit3Runner.java
+++ src/test/java/next/reflection/Junit3Runner.java
@@ -1,11 +1,18 @@
 package next.reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Junit3Runner {
-    @Test
-    public void runner() throws Exception {
-        Class clazz = Junit3Test.class;
 
+    private static final Logger log = LoggerFactory.getLogger(Junit3Runner.class);
+
+    @Test
+    @DisplayName("Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다.")
+    public void runner() throws Exception {
+        Class clazz = Question.class;
+        log.info("clazz: {}", clazz);
     }
 }
